4c8520b5643f0fd4485eedee31e696a361d21b6c
Daniel Kobrzycki <daniel.kobrzycki@gmail.com>
2022-12-03 21:21:32 +0100

    created SingleCard component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
 
-  const cardImages = [
-    { "src": "/img/helmet-1.png" },
-    { "src": "/img/potion-1.png" },
-    { "src": "/img/ring-1.png" },
-    { "src": "/img/scroll-1.png" },
-    { "src": "/img/shield-1.png" },
-    { "src": "/img/sword-1.png" },  
-  ]
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
 
 function App() {
-
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
 
@@ -19,18 +19,24 @@ function App() {
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }))
+      .map((card) => ({ ...card, id: Math.random() }));
 
-    setCards(shuffledCards)
-    setTurns(0)
-  }
+    setCards(shuffledCards);
+    setTurns(0);
+  };
 
-  console.log(cards, turns)
+  console.log(cards, turns);
 
   return (
     <div className="App">
       <h1>Magic Match</h1>
+
       <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+      {cards.map((card) => (
+        <SingleCard key={card.id} card={card} />
+          ))}
+      </div>
     </div>
   );
 }
